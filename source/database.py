01a50b0b6a446af1e6e9e1268107bbb0c8ef4102
lee-aaron <sphinxer34@yahoo.com>
2017-12-20 07:18:48 +0000

    Changed account info

--- source/database.py
+++ source/database.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 import pymysql
+import account
 
 class Database:
 
     def __init__(self):
-        self.db = pymysql.connect("anime-storage.cfavnxuxzcws.us-east-1.rds.amazonaws.com",
-            "admin","mahouka-magic","anime_storage", charset="utf8mb4")
+        a = Account()
+        self.db = pymysql.connect(a.getLink(),
+            a.getUser(),a.getPass(),a.getDB(), charset="utf8mb4")
         self.cursor = self.db.cursor()
         # Use below line in MySQL to create the table
         # CREATE TABLE IF NOT EXISTS USERS (USERHASH VARCHAR(191), ANIME VARCHAR(191), SCORE INT(10), UNIQUE (USERHASH, ANIME));
